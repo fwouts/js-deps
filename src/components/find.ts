4ca3181912d142cc7f4d8310b31d737a89c9c44d
Francois Wouts <f@zenc.io>
2018-07-11 13:11:09 +1000

    Add minimal comments

--- src/components/find.ts
+++ src/components/find.ts
@@ -3,6 +3,7 @@ import { analyzeDirectory } from "../deps/analyze";
 
 let sourceDirectory = process.argv[2];
 
+// Analyze a directory and group circular dependencies into components.
 let registry = analyzeDirectory(sourceDirectory);
 const { components, adjacencyList } = scc(registry.deps);
 for (const componentIndex of Object.keys(components)) {
@@ -10,5 +11,7 @@ for (const componentIndex of Object.keys(components)) {
   for (const nodeIndex of components[componentIndex]) {
     paths.push(registry.paths[nodeIndex]);
   }
+  // The paths in each component have circular dependencies and cannot be split
+  // into smaller pieces.
   console.log(componentIndex, paths);
 }
